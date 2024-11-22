e09fcfba8a97a5a8ccfdf6027ebb4a7f02fff9dc
Sadiajaved-113 <sadiajaved010@gmail.com>
2024-11-22 09:33:17 -0800

    Initial Commit

---
+++ resp.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-list");
+const rightNav = document.querySelector(".right-nav");
+
+if (burger && nav && rightNav) {
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("v-class-resp");
+        rightNav.classList.toggle("v-class-resp");
+        navbar.classList.toggle('h-nav-resp');
+        burger.classList.toggle("toggle");
+    });
+} else {
+    console.error("Elements not found!");
+}
