8b8b10861f8dd485b5f79cff63fc84c1d96775f3
Sadiajaved-113 <sadiajaved010@gmail.com>
2024-11-23 00:30:47 -0800

    Update Code

--- resp.js
+++ resp.js
@@ -1,12 +1,13 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav-list");
+const navbar = document.querySelector(".navbar");
 const rightNav = document.querySelector(".right-nav");
 
 if (burger && nav && rightNav) {
     burger.addEventListener("click", () => {
         nav.classList.toggle("v-class-resp");
         rightNav.classList.toggle("v-class-resp");
-        navbar.classList.toggle('h-nav-resp');
+        navbar.classList.toggle("h-nav-resp");
         burger.classList.toggle("toggle");
     });
 } else {
